048fcf4789afdfcb6f08a460ebb4c934ca9062d4
okolenchenko <kololesya@gmail.com>
2025-06-06 15:59:31 +0200

    refactoring User and UserMapper

--- src/main/java/com/solvd/laba/models/User.java
+++ src/main/java/com/solvd/laba/models/User.java
@@ -1,43 +1,24 @@
 package com.solvd.laba.models;
 
-public class User {
+import lombok.Data;
 
-    private int userId;
+@Data
+public class User {
+    private int id;
     private String username;
     private String password;
-    private String fullName;
-
-    public int getUserId() {
-        return userId;
-    }
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getFullName() {
-        return fullName;
-    }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+    private String firstName;
+    private String lastName;
+    private String zipCode;
 
     @Override
     public String toString() {
         return "User{" +
-                "userId=" + userId +
+                "id=" + id +
                 ", username='" + username + '\'' +
-                ", fullName='" + fullName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", zipCode='" + zipCode + '\'' +
                 '}';
     }
 }
